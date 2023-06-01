9380bb1eacfc4d5b723dda8f95639a36f53d7021
englishtube <129168257+englishtube@users.noreply.github.com>
2023-06-01 15:58:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash, send_from_directory, url_for
+from flask import Flask, request, render_template, flash, send_from_directory, url_for, redirect
 import os
 import whisper
 import shutil
@@ -85,7 +85,7 @@ def subtitle_gen():
         )
           
         data = {"status":"success", "video" : filename, "english_srt_file" : english_srt_file}
-        print("Data : ",data)
+        print("Data : ", data)
         flash("english: English Subtitle is generated successfully !!", "success")
         return render_template('index.html') 
 
@@ -109,15 +109,15 @@ def subtitle_gen():
           audios, srt_only, output_dir, lambda audio_path: model.transcribe(audio_path, fp16=False)
         )
 
-        json_d = {"status":"success", "video" : filename, "english_srt_file" : english_srt_file}
-        print("Data : ",data)
+        data = {"status":"success", "video" : filename, "english_srt_file" : english_srt_file}
+        print("Data : ", data)
         flash("english: English Subtitle is generated successfully !!", "success")
         return render_template('index.html') 
     except Exception as e:
       data = {"status":"failed","error":str(e)}
       print("Data : ",data)
-      flash("englisherror: English Subtitle is not generated!!", "error")
-      return render_template('index.html') 
+      flash("enerror: English Subtitle is not generated!!", "error")
+      return redirect('/')
 
 @app.route("/chainesesrt_gen",methods=['POST','GET'])
 def chainese():
@@ -141,12 +141,12 @@ def chainese():
     data = {"status":"success", "chainese_srt_file" : chainese_srt_file}
     print("Data : ", data)
     flash("chinese: Chinese Subtitle is generated successfully !!", "success")
-    return render_template('index.html', chinese_file=chainese_srt_file) 
+    return render_template('index.html') 
   except Exception as e:
     data = {"status":"failed","error":str(e)}
     print("Data : ", data)
-    flash("chineseerror: Chinese Subtitle is not generated !!", "error")
-    return render_template('index.html') 
+    flash("cherror: Chinese Subtitle is not generated !!", "error")
+    return redirect('/')
 
 @app.route("/japanesesrt_gen",methods=['POST','GET'])
 def japanese():
@@ -174,8 +174,8 @@ def japanese():
   except Exception as e:
     data = {"status":"failed","error":str(e)}
     print("Data : ", data)
-    flash("japaneseerror: Japanese Subtitle is not generated !!", "error")
-    return render_template('index.html') 
+    flash("japerror: Japanese Subtitle is not generated !!", "error")
+    return redirect('/')
 
 @app.route("/koreansrt_gen",methods=['POST','GET'])
 def korean():
@@ -203,8 +203,8 @@ def korean():
   except Exception as e:
     data = {"status":"failed","error":str(e)}
     print("Data : ", data)
-    flash("koreanerror: Korean Subtitle is not generated !!", "error")
-    return render_template('index.html') 
+    flash("koerror: Korean Subtitle is not generated !!", "error")
+    return redirect('/')
 
 @app.route('/download/english/srt')
 def downloadenglishsrt():
@@ -214,7 +214,7 @@ def downloadenglishsrt():
     return send_from_directory(srt_folder_path, filename)
   else:
     flash("englishfile: English Subtitle File is not found !!", "error")
-  return render_template('index.html') 
+  return redirect('/')
 
 @app.route('/download/chainese/srt')
 def downloadchainesesrt():
@@ -224,7 +224,7 @@ def downloadchainesesrt():
     return send_from_directory(srt_folder_path, filename)
   else:
     flash("chinesefile: Chinese Subtitle File is not found !!", "error")
-  return render_template('index.html') 
+  return redirect('/')
 
 @app.route('/download/japanese/srt')
 def downloadjapanesesrt():
@@ -234,7 +234,7 @@ def downloadjapanesesrt():
     return send_from_directory(srt_folder_path, filename)
   else:
     flash("japanesefile: Japanese Subtitle File is not found !!", "error")
-  return render_template('index.html') 
+  return redirect('/')
 
 @app.route('/download/korean/srt')
 def downloadkoreansrt():
@@ -244,7 +244,67 @@ def downloadkoreansrt():
     return send_from_directory(srt_folder_path, filename)
   else:
     flash("koreanfile: Korean Subtitle File is not found !!", "error")
-  return render_template('index.html') 
+  return redirect('/')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+
+
+# Subtitle Generator using video url
+# @app.route("/videourl_subtitle",methods=['POST','GET'])
+# def url_subtitle():
+#   filename = ""
+#   data = ""
+#   json_data = ""
+#   if(request.method == 'POST'):
+#     try: 
+#       isExistinput = os.path.exists(video_folder_path)
+#       if(isExistinput == True):
+#         shutil.rmtree(video_folder_path)
+#         os.mkdir(video_folder_path)
+#       elif(isExistinput == False):
+#         os.mkdir(video_folder_path)
+
+#       video_url = request.form['video_url']
+#       downloaded_filename = os.path.join(app.config['VIDEO_UPLOAD_FOLDER'], "downloaded_video.mp4")
+#       download_videos = subtitle.download_video(video_url, downloaded_filename)
+    
+#       isExistinput = os.path.exists(srt_folder_path)
+#       if(isExistinput == True):
+#           shutil.rmtree(srt_folder_path)
+#           os.mkdir(srt_folder_path)
+#       elif(isExistinput == False):
+#           os.mkdir(srt_folder_path)
+
+#       model_name = "small"
+#       output_dir = srt_folder_path
+#       srt_only = True
+
+#       if model_name.endswith(".en"):
+#         warnings.warn(
+#             f"{model_name} is an English-only model, forcing English detection.")
+#         args["language"] = "en"
+
+#       model = whisper.load_model(model_name)
+
+#       isExistinput = os.path.exists(extracted_folder_path)
+#       if(isExistinput == True):
+#           shutil.rmtree(extracted_folder_path)
+#           os.mkdir(extracted_folder_path)
+#       elif(isExistinput == False):
+#           os.mkdir(extracted_folder_path)
+
+#       audios = subtitle.get_audio(download_videos,extracted_folder_path)
+#       english_srt_file = subtitle.get_subtitles(
+#         audios, srt_only, output_dir, lambda audio_path: model.transcribe(audio_path, fp16=False)
+#       )
+
+#       json_d = {"status":"success", "video" : filename, "english_srt_file" : english_srt_file}
+#       print("Data : ",data)
+#       flash("english: English Subtitle is generated successfully !!", "success")
+#       return render_template('index.html') 
+#     except Exception as e:
+#       data = {"status":"failed","error":str(e)}
+#       print("Data : ",data)
+#       flash("englisherror: English Subtitle is not generated!!", "error")
+#       return render_template('index.html') 
